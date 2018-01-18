397af7cf8a59619afbbf7e841a0bc6e41db364a7
sipp <jgilsonsi@gmail.com>
2018-01-18 16:26:57 -0200

    Add new script to send RTP during calls

--- gen_call_csv.sh
+++ gen_call_csv.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 #Generate csv file: ./gen_call_csv.sh > call.csv 
-#File struct: 1000;192.168.9.127;[authentication username=1000 password=asterisk1000];1001;
+#File struct: 1000;192.168.9.206;[authentication username=1000 password=123mudar1000];1001;
 #	      extension;server;[authentication username=username password=password];call_target; 
 
 
 #TYPE: RANDOM, SEQUENTIAL AND USER
 echo "SEQUENTIAL" 
 for i in `seq 1000 1010`; do
-        echo "$i;192.168.9.127;[authentication username=$i password=asterisk$i];1001;"
+        echo "$i;192.168.9.206;[authentication username=$i password=123mudar$i];$(($i+1));"
 done
--- gen_register_csv.sh
+++ gen_register_csv.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 #Generate csv file: ./gen_register_csv.sh > register.csv 
-#File struct: 1000;192.168.9.127;[authentication username=1000 password=asterisk1000];
+#File struct: 1000;192.168.9.206;[authentication username=1000 password=123mudar1000];
 #	      extension;server;[authentication username=username password=password]; 
 
 
 #TYPE: RANDOM, SEQUENTIAL AND USER
 echo "SEQUENTIAL" 
 for i in `seq 1000 1010`; do
-        echo "$i;192.168.9.127;[authentication username=$i password=asterisk$i];"
+        echo "$i;192.168.9.206;[authentication username=$i password=123mudar$i];"
 done
